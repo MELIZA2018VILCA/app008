a499d82f6fd99dbd9784727d55c60c0332deeb41
meliza <201913306k@utea.edu.pe>
2022-10-31 20:14:08 -0500

    alert2_3

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -27,13 +27,79 @@ class AlertPage extends StatelessWidget {
       },
     );
   }
-  showAlert(BuildContext context) {
+
+  showMyAlert2(BuildContext context) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          
-          title: Text("Blog post published"),
+          title: Column(
+            children: [
+              Row(
+                children: [
+                  Image.network(
+                    'assets/images/aler2.png',
+                    width: 70,
+                    height: 70,
+                    fit: BoxFit.contain,
+                  ),
+                ],
+              ),
+              Text("blog  post published "),
+            ],
+          ),
+          backgroundColor: Colors.white,
+          content: Text("you`re video has finished uploading and is live"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text(
+                "              Cancelar                 ",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0), fontSize: 15),
+              ),
+              style: OutlinedButton.styleFrom(
+                primary: Color.fromARGB(255, 255, 255, 255),
+                side: BorderSide(color: Color.fromARGB(255, 15, 12, 15)),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                "              Confirm               ",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 241, 239, 239), fontSize: 15),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                    Color.fromARGB(255, 152, 54, 244)),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  showMyAlert3(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Column(
+            children: [
+              Row(
+                children: [
+                  Image.network('assets/images/smile.jpg',
+                 
+                      width: 100, height: 300),
+                ],
+              ),
+              Text("blog  post published "),
+            ],
+          ),
           backgroundColor: Colors.white,
           content: Text(
               "this blog post has been published. team members will be able to edit this post and republish changes"),
@@ -53,64 +119,6 @@ class AlertPage extends StatelessWidget {
       },
     );
   }
-  showAlerta(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("your videos has been uploaded"),
-          backgroundColor: Colors.white,
-          content: Text(
-              "you`re video has finished uploading and is live"),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text("Cancelar"),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text("Aceptar"),
-            ),
-          ],
-          
-        );
-      },
-    );
-  }
-showwAlerta(BuildContext context) {
-    showDialog(
-                              context: context,
-                              builder: (BuildContext context) => AlertDialog(
-                                title: null,
-                                content: Container(
-                                  height: 10.0,
-                                  child: Column(
-                                    children: [
-                                      Container(
-                                        width: 10.0,
-                                        child: Image(image: AssetImage("assets/images/aler2.png"),)
-                                      ),
-                                      Container(
-                                        width:  1,
-                                        child: Text(
-                                          "Success",
-                                          style: TextStyle(
-                                              fontFamily: 'HelveticaNueue',
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 16.0,
-                                              height: 1.5),
-                                          textAlign: TextAlign.center,
-                                        )),
-                                    ],
-                                  ),
-                                )
-
-                              ));
-  }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -133,28 +141,21 @@ showwAlerta(BuildContext context) {
             SizedBox(
               height: 15.0,
             ),
-            
             ElevatedButton(
               onPressed: () {
-                showAlert(context);
+                showMyAlert2(context);
               },
               child: Text("Alert2"),
             ),
-             SizedBox(
+            SizedBox(
               height: 15.0,
             ),
             ElevatedButton(
               onPressed: () {
-                showAlerta(context);
+                showMyAlert3(context);
               },
               child: Text("Alert3"),
             ),
-            ElevatedButton(
-              onPressed: () {
-                showwAlerta(context);
-              },
-              child: Text("prueba"),
-            )
           ],
         ),
       ),
